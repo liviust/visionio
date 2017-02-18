d085b46ca084e5a1dafb9400e6f41510cb52c548
Ming <a@ufoym.com>
2017-02-18 22:08:35 +0800

    MINOR

--- app.py
+++ app.py
@@ -5,21 +5,31 @@ import os
 from datetime import datetime
 from flask import Flask, render_template, jsonify, request
 
+# ----------------------------------------------------------------------------
+
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
+# ----------------------------------------------------------------------------
+
+def allow(filename, allowed=['png', 'jpg', 'jpeg', 'gif']):
+    return '.' in filename and filename.rsplit(
+        '.', 1)[1].lower() in allowed
+
+# ----------------------------------------------------------------------------
 
 @app.route('/')
 def basic():
     return render_template('basic.html')
 
+# ----------------------------------------------------------------------------
 
 @app.route('/upload', methods=['POST'])
 def upload():
     if request.method == 'POST':
         file = request.files['file']
-        if file and allowed_file(file.filename):
+        if file and allow(file.filename):
             now = datetime.now()
             filename = os.path.join(
                 app.config['UPLOAD_FOLDER'],
@@ -30,10 +40,9 @@ def upload():
             return jsonify({'success': True})
     return jsonify({'success': False})
 
-
-def allowed_file(filename, allowed=['png', 'jpg', 'jpeg', 'gif']):
-    return '.' in filename and filename.rsplit(
-        '.', 1)[1].lower() in allowed
+# ----------------------------------------------------------------------------
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+# ----------------------------------------------------------------------------
